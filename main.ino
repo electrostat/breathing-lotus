c6b5260e90a9415a067c0df5771ee3ba7624f65f
electrostat <agbyaj@gmail.com>
2018-03-04 21:40:54 -0500

    Change to Button Click Activation
    - change time
    - clear out unused code
    - make breathing routine be on button click
    - change states

--- main.ino
+++ main.ino
@@ -1,4 +1,3 @@
-// This #include statement was automatically added by the Particle IDE.
 #include <InternetButton.h>
 
 // this lamp will pulse at a rate of 5 breaths/min (1 cycle / 12 s)
@@ -11,22 +10,10 @@ float brightnessfactor; // how bright the LEDs will be, as a fraction of full br
 
 int state;  // The current state of the device.
             // 0: lights off
-            // 1: reminder lights and sound on
-            // 2: waiting for you to pick it up
-            // 3: entering breathing pattern
-            // 4: exiting breathing pattern
-
-int moving=0;   // assume it is not moving to start
-int movingThreshold=100;
-
-int timeToGoOff=14;  // in hours
-int timeZone=-4; // est
-
-int lastChecked=0;
-int checkInterval=300000;
+            // 1: breathing pattern on
 
 int mindfulTime;
-int mindfulInterval=300000; // 5 minutes of mindful breathing
+int mindfulInterval=600000; // 5 minutes of mindful breathing
 
 InternetButton b = InternetButton(); // We will refer to our Internet Button as b
 
@@ -48,74 +35,27 @@ void setup() {
 void loop() {
     int t=millis();
     
-    if (state==0) {
-        // wait checkInterval ms, then check the loop
-        if (t>lastChecked+checkInterval) {
-            if (checkTime()==1) {
-                state=1;
-            }
-            else {
-                state=0;
-            }
-        }
-    }
-    else if (state==1) {
-        // play reminder lights and sound, progress to 2
-        reminder();
-        state=2;
-    }
-    else if (state==2) {
-        // wait for you to pick it up
-        if (isItMoving()==1) {
+    if (b.buttonOn(3)) {
+        if (state==0) {
+            state = 1;
             mindfulTime=millis();
-            Particle.publish("start",NULL,60,PRIVATE);
-            state=3;
+        } else if (state==1) {
+            state = 0;
         }
     }
-    else if (state==3) {
+    
+    if (state==1) {
         if (t<mindfulTime+mindfulInterval) {
             breathingPattern();
-        }
-        else {
+        } else {
             finished();
             Particle.publish("end",NULL,60,PRIVATE);
             state=4;
         }
-    }
-    else if (state==4) {
-        // wait an hour
-        if (t>mindfulTime+3600000) {
-            state=0;
-        }
-    }
-    
-    if (checkForShake()==1) {
-        advanceState();
-    }
-    
-}
-    
-int checkForShake() {
-    if (abs(b.readX())>100) {
-        delay(500);
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
-
-void advanceState() {
-    if (state==4) {
-        mindfulTime=millis()-3600000;
+    } else {
         b.allLedsOff();
-        state=0;
-    }
-    else {
-        state++;
     }
 }
-
 
 void breathingPattern() {
     int breatht = millis() % breathperiod;
@@ -125,16 +65,6 @@ void breathingPattern() {
     int brightnessvalueint = brightnessfactor * 100;
     
     b.allLedsOn(brightnessvalueint, brightnessvalueint/2, 0);
-}
-
-
-int isItMoving() {
-    if (abs(b.readZ())>movingThreshold) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
 }
 
 int checkTime() {
@@ -150,19 +80,6 @@ int checkTime() {
     lastChecked=millis();
 }
 
-void reminder() {
-    b.allLedsOn(0,100,100);
-    b.playSong("B5,8,G4,4,");
-    delay(500);
-    b.allLedsOff();
-    delay(500);
-    b.allLedsOn(0,100,100);
-    delay(500);
-    b.allLedsOff();
-    delay(500);
-    b.allLedsOn(100,100,0);
-}
-
 void finished() {
     b.playSong("D4,4,G4,2,");
     // fade to black
@@ -172,12 +89,4 @@ void finished() {
         b.allLedsOn(i,i,i);
     }
     b.allLedsOff();
-}
-
-int manualState(String command) {
-    // sets state manually through a Particle.function
-    char inputStr[64];
-    command.toCharArray(inputStr,64);
-    state=atoi(inputStr);
-    return state;
 }
